2309609b0282f41186d2986a82278fd7773b963d
infinia-yzl <dev@infinia.space>
2024-06-24 15:02:04 +0800

    Minor refactors & content additions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
-'use client';
-
-import React, { useState } from 'react';
+import React from 'react';
 import { SiGithub, SiLinkedin } from 'react-icons/si';
 import { MdEmail } from 'react-icons/md';
 import { HiOutlineExternalLink } from 'react-icons/hi';
@@ -23,12 +21,14 @@ const MSG_INTRO: Message = {
   },
 };
 
-const TECHNOLOGIES = [
+const TECHNOLOGIES: { name: string }[] = [
   { name: 'React' },
   { name: 'NextJS' },
   { name: 'NestJS' },
   { name: 'Typescript' },
   { name: 'Javascript' },
+  { name: 'Expo (React Native)' },
+  { name: 'Supabase' },
   { name: 'REST' },
   { name: 'GraphQL' },
   { name: 'Jest' },
@@ -36,6 +36,7 @@ const TECHNOLOGIES = [
   { name: 'Cypress' },
   { name: 'Playwright' },
   { name: 'SQL' },
+  { name: 'PostgreSQL' },
   { name: 'TypeORM' },
   { name: 'Sequelize' },
   { name: 'KnexJS' },
@@ -43,6 +44,7 @@ const TECHNOLOGIES = [
   { name: 'NodeJS' },
   { name: 'BunJS' },
   { name: 'Vite' },
+  { name: 'HTML' },
   { name: 'Material UI' },
   { name: 'Tailwind CSS' },
   { name: 'Blockchain' },
@@ -90,8 +92,6 @@ const getTechnologiesFontSize = (index: number) => {
 // ];
 
 export default function Home() {
-  const [message] = useState(MSG_INTRO);
-
   // Define the base size for the sun or galaxy core
   const CORE_SIZE_BASE = 15;
 
@@ -105,48 +105,50 @@ export default function Home() {
   // Function to determine the correct rotation animation based on index
   const getRotationAnimation = (index: number) => (index % 2 === 0 ? 'spin-cw 120s linear infinite' : 'spin-ccw 120s linear infinite');
 
+  const staticOrbitSection = () => (
+    <div className="w-full md:w-1/2 relative flex items-center justify-center">
+      {/* Orbits */}
+      {ORBIT_SIZES.map((size, index) => {
+        const animationDelay = `${(PULSE_ANIMATION_DURATION / ORBIT_SIZES.length) * index * 0.1}s`;
+        const rotationAnimation = getRotationAnimation(index);
+        return (
+          <div
+            /* eslint-disable-next-line react/no-array-index-key */
+            key={`orbit-${index}`}
+            className="orbit absolute rounded-full border"
+            style={{
+              animation: `${PULSE_ANIMATION} ${animationDelay}, ${rotationAnimation}`,
+              width: `${size}vw`, // Adjust max value as needed
+              height: `${size}vw`, // Adjust max value as needed
+              maxWidth: `${(index + 1) * 13.5}vw`,
+              maxHeight: `${(index + 1) * 13.5}vw`,
+            }}
+          />
+        );
+      })}
+
+      {/* Sun or Galaxy Core */}
+      <div
+        className="z-10 rounded-full flex items-center justify-center"
+        style={{
+          width: `${CORE_SIZE_BASE}vw`,
+          height: `${CORE_SIZE_BASE}vw`,
+        }}
+      >
+        <h1 className="text-white font-thin" style={{ fontSize: `${CORE_SIZE_BASE / 12}vw` }}>
+          infinia.space
+        </h1>
+      </div>
+    </div>
+  );
+
   return (
     <main>
       <section className="h-screen flex flex-col md:flex-row justify-center items-center">
-        <div className="w-full md:w-1/2 relative flex items-center justify-center">
-          {/* Orbits */}
-          {ORBIT_SIZES.map((size, index) => {
-            const animationDelay = `${(PULSE_ANIMATION_DURATION / ORBIT_SIZES.length) * index * 0.1}s`;
-            const rotationAnimation = getRotationAnimation(index);
-            return (
-              // eslint-disable-next-line react/no-array-index-key
-              <React.Fragment key={index}>
-                <div
-                  /* eslint-disable-next-line react/no-array-index-key */
-                  key={index}
-                  className="orbit absolute rounded-full border"
-                  style={{
-                    animation: `${PULSE_ANIMATION} ${animationDelay}, ${rotationAnimation}`,
-                    width: `${size}vw`, // Adjust max value as needed
-                    height: `${size}vw`, // Adjust max value as needed
-                    maxWidth: `${(index + 1) * 13.5}vw`,
-                    maxHeight: `${(index + 1) * 13.5}vw`,
-                  }}
-                />
-              </React.Fragment>
-            );
-          })}
-          {/* Sun or Galaxy Core */}
-          <div
-            className="z-10 rounded-full flex items-center justify-center"
-            style={{
-              width: `${CORE_SIZE_BASE}vw`,
-              height: `${CORE_SIZE_BASE}vw`,
-            }}
-          >
-            <h1 className="text-white font-thin" style={{ fontSize: `${CORE_SIZE_BASE / 12}vw` }}>
-              infinia.space
-            </h1>
-          </div>
-        </div>
+        {staticOrbitSection()}
         <div className="w-full md:w-1/2 p-4 mt-24 md:mt-0">
-          <h2 className="text-xl mb-2">{message.title}</h2>
-          <p className="mb-2">{message.body}</p>
+          <h2 className="text-xl mb-2">{MSG_INTRO.title}</h2>
+          <p className="mb-2">{MSG_INTRO.body}</p>
           <div className="font-thin border-l-2 border-gray-700 pl-2 mb-2 max-w-screen-md">
             {TECHNOLOGIES.map((tech, index) => {
               const fontSize = getTechnologiesFontSize(index);
@@ -169,12 +171,12 @@ export default function Home() {
               className="inline-flex items-center rounded-full border-2 border-white bg-black ml-2 py-2 px-4 hover:bg-white hover:text-black transition duration-400 ease-in-out focus:outline-none focus:ring-2 focus:ring-offset-white focus:ring-opacity-50"
             >
               <a
-                href={message.highlight.url}
+                href={MSG_INTRO.highlight.url}
                 target="_blank"
                 rel="noreferrer"
                 className="flex items-center"
               >
-                <span>{message.highlight.title}</span>
+                <span>{MSG_INTRO.highlight.title}</span>
                 <HiOutlineExternalLink className="ml-2" />
               </a>
             </button>
@@ -183,12 +185,22 @@ export default function Home() {
           {/* CTA */}
           <div className="flex space-x-4">
             <button type="button" aria-label="github">
-              <a href="https://github.com/infinia-yzl" target="_blank" rel="noreferrer" aria-label="github">
+              <a
+                href="https://github.com/infinia-yzl"
+                target="_blank"
+                rel="noreferrer"
+                aria-label="github"
+              >
                 <SiGithub className="text-4xl" />
               </a>
             </button>
             <button type="button" aria-label="linkedin">
-              <a href="https://www.linkedin.com/in/infinia/" target="_blank" rel="noreferrer" aria-label="linkedin">
+              <a
+                href="https://www.linkedin.com/in/infinia/"
+                target="_blank"
+                rel="noreferrer"
+                aria-label="linkedin"
+              >
                 <SiLinkedin className="text-4xl" />
               </a>
             </button>
